6eeaa4c934aac2b23b841f6e5a49d425beb5af10
csaksith <saksitca@mail.uc.edu>
2025-04-05 20:54:17 -0400

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListComponent } from './feature/user/user-list/user-list.component';
+import { ProductCreateComponent } from './feature/product/product-create/product-create.component';
+import { ProductDetailComponent } from './feature/product/product-detail/product-detail.component';
+import { ProductEditComponent } from './feature/product/product-edit/product-edit.component';
+import { ProductListComponent } from './feature/product/product-list/product-list.component';
+import { RequestCreateComponent } from './feature/request/request-create/request-create.component';
+import { RequestDetailComponent } from './feature/request/request-detail/request-detail.component';
+import { RequestEditComponent } from './feature/request/request-edit/request-edit.component';
+import { RequestListComponent } from './feature/request/request-list/request-list.component';
+import { UserCreateComponent } from './feature/user/user-create/user-create.component';
+import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
+import { UserEditComponent } from './feature/user/user-edit/user-edit.component';
+import { VendorCreateComponent } from './feature/vendor/vendor-create/vendor-create.component';
+import { VendorDetailComponent } from './feature/vendor/vendor-detail/vendor-detail.component';
+import { VendorEditComponent } from './feature/vendor/vendor-edit/vendor-edit.component';
+import { VendorListComponent } from './feature/vendor/vendor-list/vendor-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+// user
+{path: 'user-list', component: UserListComponent },
+{path: 'user-create', component: UserCreateComponent },
+{path: 'user-edit', component: UserEditComponent },
+{path: 'user-detail', component: UserDetailComponent },
+// vendor
+{path: 'vendor-list', component: VendorListComponent },
+{path: 'vendor-create', component: VendorCreateComponent },
+{path: 'vendor-edit', component: VendorEditComponent },
+{path: 'vendor-detail', component: VendorDetailComponent },
+// request
+{path: 'request-list', component: RequestListComponent },
+{path: 'request-create', component: RequestCreateComponent },
+{path: 'request-edit', component: RequestEditComponent },
+{path: 'request-detail', component: RequestDetailComponent },
+// product
+{path: 'product-list', component: ProductListComponent },
+{path: 'product-create', component: ProductCreateComponent },
+{path: 'product-edit', component: ProductEditComponent },
+{path: 'product-detail', component: ProductDetailComponent },
+// default route
+{ path: '', redirectTo: 'user-list', pathMatch: 'full' }, // redirect to user list
+{path: '**', redirectTo: 'user-list' } // wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './feature/user/user-list/user-list.component';
+import { UserCreateComponent } from './feature/user/user-create/user-create.component';
+import { UserEditComponent } from './feature/user/user-edit/user-edit.component';
+import { UserDetailComponent } from './feature/user/user-detail/user-detail.component';
+import { VendorListComponent } from './feature/vendor/vendor-list/vendor-list.component';
+import { VendorCreateComponent } from './feature/vendor/vendor-create/vendor-create.component';
+import { VendorEditComponent } from './feature/vendor/vendor-edit/vendor-edit.component';
+import { VendorDetailComponent } from './feature/vendor/vendor-detail/vendor-detail.component';
+import { RequestListComponent } from './feature/request/request-list/request-list.component';
+import { RequestCreateComponent } from './feature/request/request-create/request-create.component';
+import { RequestEditComponent } from './feature/request/request-edit/request-edit.component';
+import { RequestDetailComponent } from './feature/request/request-detail/request-detail.component';
+import { ProductListComponent } from './feature/product/product-list/product-list.component';
+import { ProductCreateComponent } from './feature/product/product-create/product-create.component';
+import { ProductEditComponent } from './feature/product/product-edit/product-edit.component';
+import { ProductDetailComponent } from './feature/product/product-detail/product-detail.component';
+import { MenuComponent } from './core/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserCreateComponent,
+    UserEditComponent,
+    UserDetailComponent,
+    VendorListComponent,
+    VendorCreateComponent,
+    VendorEditComponent,
+    VendorDetailComponent,
+    RequestListComponent,
+    RequestCreateComponent,
+    RequestEditComponent,
+    RequestDetailComponent,
+    ProductListComponent,
+    ProductCreateComponent,
+    ProductEditComponent,
+    ProductDetailComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
