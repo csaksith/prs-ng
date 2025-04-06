6767b5561535bed52647d90cf3b255e3aa544e72
csaksith <saksitca@mail.uc.edu>
2025-04-06 15:44:20 -0400

    add vendor-detail with working detail

--- src/app/feature/vendor/vendor-detail/vendor-detail.component.ts
+++ src/app/feature/vendor/vendor-detail/vendor-detail.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { Vendor } from '../../../model/vendor.model';
+import { VendorService } from '../../../service/vendor.service';
 
 @Component({
   selector: 'app-vendor-detail',
   standalone: false,
   templateUrl: './vendor-detail.component.html',
-  styleUrl: './vendor-detail.component.css'
+  styleUrl: './vendor-detail.component.css',
 })
-export class VendorDetailComponent {
+export class VendorDetailComponent implements OnInit, OnDestroy {
+  title: string = 'Vendor-Detail';
+  vendorId!: number;
+  vendor!: Vendor;
+  subscription!: Subscription;
 
+  constructor(
+    private vendorSvc: VendorService,
+    private router: Router,
+    private actRoute: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    // get vendorId from URL
+    this.actRoute.params.subscribe((parms) => {
+      this.vendorId = parms['id'];
+      // get vendor by id
+      this.subscription = this.vendorSvc.getById(this.vendorId).subscribe({
+        next: (resp) => {
+          this.vendor = resp;
+        },
+        error: (err) => {
+          console.error('Error fetching vendor:', err);
+        },
+      });
+    });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  deleteVendor() {
+    this.vendorSvc.delete(this.vendorId).subscribe({
+      next: (resp) => {
+        this.router.navigateByUrl('/vendor-list');
+      },
+      error: (err) => {
+        console.error('Error deleting vendor:', err);
+      },
+    });
+  }
 }
--- src/app/feature/vendor/vendor-list/vendor-list.component.ts
+++ src/app/feature/vendor/vendor-list/vendor-list.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Vendor } from '../../../model/vendor.model';
+import { VendorService } from '../../../service/vendor.service';
 
 @Component({
   selector: 'app-vendor-list',
   standalone: false,
   templateUrl: './vendor-list.component.html',
-  styleUrl: './vendor-list.component.css'
+  styleUrl: './vendor-list.component.css',
 })
-export class VendorListComponent {
+export class VendorListComponent implements OnInit, OnDestroy {
+  title: string = 'Vendor-List';
+  vendors!: Vendor[];
+  subscription!: Subscription;
+  constructor(private vendorSvc: VendorService) {}
+  ngOnInit(): void {
+    this.subscription = this.vendorSvc.list().subscribe((resp) => {
+      this.vendors = resp;
+    });
+  }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  delete(id: number) {
+    this.subscription = this.vendorSvc.delete(id).subscribe({
+      next: () => {
+        this.subscription = this.vendorSvc.list().subscribe((resp) => {
+          this.vendors = resp;
+        });
+      },
+      error: (err) => {
+        console.log('Error deleting vendor');
+        alert('Error deleting vendor');
+      },
+    });
+  }
 }
