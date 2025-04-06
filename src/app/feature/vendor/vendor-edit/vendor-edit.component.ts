e751d0e6c1780d029d3c1cacd061430bd98d1cb4
csaksith <saksitca@mail.uc.edu>
2025-04-06 16:59:43 -0400

    complete vendor-edit, redirect to vendor-list pending

--- src/app/feature/vendor/vendor-edit/vendor-edit.component.ts
+++ src/app/feature/vendor/vendor-edit/vendor-edit.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { Vendor } from '../../../model/vendor.model';
+import { VendorService } from '../../../service/vendor.service';
 
 @Component({
   selector: 'app-vendor-edit',
   standalone: false,
   templateUrl: './vendor-edit.component.html',
-  styleUrl: './vendor-edit.component.css'
+  styleUrl: './vendor-edit.component.css',
 })
-export class VendorEditComponent {
+export class VendorEditComponent implements OnInit, OnDestroy {
+  title: string = 'Vendor-Edit';
+  vendorId!: number;
+  vendor!: Vendor;
+  subscription!: Subscription;
+  constructor(
+    private vendorSvc: VendorService,
+    private router: Router,
+    private actRoute: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    // get the vendorId from the URL
+    this.actRoute.params.subscribe((parms) => {
+      this.vendorId = parms['id'];
+      // get the vendor by id
+      this.subscription = this.vendorSvc.getById(this.vendorId).subscribe({
+        next: (resp) => {
+          this.vendor = resp;
+        },
+        error: (err) => {
+          console.error('Error fetching vendor:', err);
+        },
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  saveVendor() {
+    this.vendorSvc.update(this.vendor).subscribe({
+      next: (resp) => {
+        this.vendor = resp;
+        this.router.navigateByUrl('/vendor-list');
+      },
+      error: (err) => {
+        console.log('Error saving vendor: ', err);
+      },
+    });
+  }
 }
