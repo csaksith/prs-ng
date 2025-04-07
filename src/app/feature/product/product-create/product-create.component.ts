93a5013a28230e8859ff86d99371035297bece1d
csaksith <saksitca@mail.uc.edu>
2025-04-07 08:52:32 -0400

    add product-create with working delete

--- src/app/feature/product/product-create/product-create.component.ts
+++ src/app/feature/product/product-create/product-create.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Product } from '../../../model/product.model';
+import { ProductService } from '../../../service/product.service';
+import { Vendor } from '../../../model/vendor.model';
+import { VendorService } from '../../../service/vendor.service';
 
 @Component({
   selector: 'app-product-create',
   standalone: false,
   templateUrl: './product-create.component.html',
-  styleUrl: './product-create.component.css'
+  styleUrl: './product-create.component.css',
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit, OnDestroy {
+  title: string = 'Product-Create';
+  newProduct: Product = new Product();
+  subscription!: Subscription;
+  vendors!: Vendor[];
 
+  constructor(
+    private productSvc: ProductService,
+    private vendorSvc: VendorService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    // populate vendors list
+    this.subscription = this.vendorSvc.list().subscribe({
+      next: (resp) => {
+        this.vendors = resp;
+      },
+      error: (err) => {
+        console.error('Product Create Error: error loading vendors', err);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  addProduct(): void {
+    this.subscription = this.productSvc.add(this.newProduct).subscribe({
+      next: (resp) => {
+        this.router.navigateByUrl('/product-list');
+      },
+      error: (err) => {
+        console.error('Product Create Error: error adding product', err);
+      },
+    });
+  }
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../model/product.model';
-const URL = 'http://localhost:8080/api/products';
+const URL = 'http://localhost:8080/api/products/';
 @Injectable({
   providedIn: 'root',
 })
@@ -10,11 +10,11 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   list(): Observable<Product[]> {
-    return this.http.get<Product[]>(URL + '/') as Observable<Product[]>;
+    return this.http.get<Product[]>(URL) as Observable<Product[]>;
   }
 
   add(product: Product): Observable<Product> {
-    return this.http.post(URL + '/', product) as Observable<Product>;
+    return this.http.post(URL, product) as Observable<Product>;
   }
 
   update(product: Product): Observable<Product> {
@@ -29,6 +29,6 @@ export class ProductService {
   }
 
   delete(id: number): Observable<any> {
-    return this.http.delete(URL + '/' + id) as Observable<Product>;
+    return this.http.delete(URL + id) as Observable<Product>;
   }
 }
